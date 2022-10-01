2d2c619bc4d5d9fb947fd9f34273eec827713b0c
pokhiii <pokhi@coloredcow.com>
2022-10-01 17:12:18 +0530

    email: via ses

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_SES_KEY'),
+        secret: env('AWS_SES_SECRET'),
+        amazon: 'https://email.eu-west-1.amazonaws.com',
+      },
+      settings: {
+        defaultFrom: 'employee@coloredcow.com',
+        defaultReplyTo: 'no-reply@coloredcow.in',
+      },
+    },
+  },
+  // ...
+});
